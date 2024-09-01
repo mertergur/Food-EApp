aa8f0481989fa4e41d20de929583aed0bf5df4f4
Mert Ergür <trewertttt@gmail.com>
2024-09-02 01:48:33 +0300

    basic homepage design created

---
+++ app/src/main/java/com/example/foodeapp/data/entity/Foods.kt
@@ -0,0 +1,12 @@
+package com.example.foodeapp.data.entity
+
+import java.io.Serializable
+
+data class Foods(
+    var yemek_id: Int,
+    var yemek_adi: String,
+    var yemek_resim_adi: String,
+    var yemek_fiyat: Int
+):Serializable{
+
+}
