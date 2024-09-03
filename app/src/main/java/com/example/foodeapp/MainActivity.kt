882b53c70e1200109c46deda2195dfe6b674dd8f
Mert Ergür <trewertttt@gmail.com>
2024-09-03 15:16:45 +0300

    added bottom nav view

--- app/src/main/java/com/example/foodeapp/MainActivity.kt
+++ app/src/main/java/com/example/foodeapp/MainActivity.kt
@@ -5,10 +5,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.foodeapp.databinding.ActivityMainBinding
+import com.example.foodeapp.ui.fragment.BasketFragment
+import com.example.foodeapp.ui.fragment.FavFragment
+import com.example.foodeapp.ui.fragment.HomePageFragment
+import com.example.foodeapp.ui.fragment.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navHostFragment.navController)
+
+
+
+/*
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.homePageFragment -> replaceFragment(HomePageFragment())
+                R.id.basketFragment -> replaceFragment(BasketFragment())
+                R.id.favFragment -> replaceFragment(FavFragment())
+                R.id.profileFragment -> replaceFragment(ProfileFragment())
+                else -> {
+                }
+            }
+            true
+        }
+        */
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.navHostFragment,fragment)
+        fragmentTransaction.commit()
     }
 }
